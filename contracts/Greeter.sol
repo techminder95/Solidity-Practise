fcdad9ed923022c06a57902330e9e0571e63394a
techminder95 <tech.miner95@gmail.com>
2024-10-20 00:10:41 +0900

    commit

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,8 +2,12 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Greeter{
-    function greete() external pure returns(string memory){
-        return "Hello, World";
+contract Greeter {
+    string private _greeting = "Hello, World!";
+    function greet() external view returns (string memory) {
+        return _greeting;
+    }
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
     }
 }
